584949d07d1934ac4db9d245ee2876d9ca27b935
beomyoonHeo <gqd0512@naver.com>
2023-12-07 14:31:32 +0900

    modified: exception 객체 추가

--- lib/exception/bluetooth_module_exception.dart
+++ lib/exception/bluetooth_module_exception.dart
@@ -7,3 +7,23 @@ class BluetoothModuleException implements Exception {
 class UnsupportedPlatformException extends BluetoothModuleException {
   UnsupportedPlatformException({String? message}) : super(message ?? 'Unsupported platform');
 }
+
+class ConnectionException extends BluetoothModuleException {
+  ConnectionException({String? message}) : super(message ?? 'Connection failed');
+}
+
+class DeviceNotFoundException extends BluetoothModuleException {
+  DeviceNotFoundException({String? message}) : super(message ?? 'Device not found');
+}
+
+class DeviceNotConnectedException extends BluetoothModuleException {
+  DeviceNotConnectedException({String? message}) : super(message ?? 'Device not connected');
+}
+
+class DeviceNotBondedException extends BluetoothModuleException {
+  DeviceNotBondedException({String? message}) : super(message ?? 'Device not bonded');
+}
+
+class DeviceNotDiscoveredException extends BluetoothModuleException {
+  DeviceNotDiscoveredException({String? message}) : super(message ?? 'Device not discovered');
+}
