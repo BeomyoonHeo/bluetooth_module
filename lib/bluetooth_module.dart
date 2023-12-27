9ac4dbb7694e1ad40395b6b8699e9447ccbf12ac
beomyoonHeo <gqd0512@naver.com>
2023-12-27 09:59:05 +0900

    feat: ble export 추가

--- lib/bluetooth_module.dart
+++ lib/bluetooth_module.dart
@@ -3,4 +3,20 @@ library bluetooth_module;
 export 'src/bluetooth_mapper.dart';
 export 'src/bluetooth_manager.dart';
 export 'setting_object/base_bluetooth_object.dart';
-export 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart' show BluetoothDevice, BluetoothDiscoveryResult;
+export 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart' hide BluetoothState, BluetoothBondState;
+export 'package:flutter_blue_plus/flutter_blue_plus.dart'
+    hide BluetoothDevice
+    show
+        OnBondStateChangedEvent,
+        BluetoothBondState,
+        OnConnectionStateChangedEvent,
+        OnCharacteristicReceivedEvent,
+        OnCharacteristicWrittenEvent,
+        OnDescriptorReadEvent,
+        OnDescriptorWrittenEvent,
+        OnDiscoveredServicesEvent,
+        OnMtuChangedEvent,
+        OnNameChangedEvent,
+        OnReadRssiEvent,
+        OnServicesResetEvent,
+        BluetoothCharacteristic;
