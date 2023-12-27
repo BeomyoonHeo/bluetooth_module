672dbdd5e4911ea830b09d8754bfa8ca74580f9e
BeomyoonHeo <55969155+BeomyoonHeo@users.noreply.github.com>
2023-12-27 10:01:56 +0900

    Features/live scanning result (#4)
    * feat: 실시간 스캔 리스트 추가
    
    * feat: ble export 추가

--- lib/extension/bluetooth_device_ext.dart
+++ lib/extension/bluetooth_device_ext.dart
@@ -1,5 +1,4 @@
 import 'package:bluetooth_module/bluetooth_module.dart';
-import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
 extension BluetoothDeviceExt on BluetoothDevice {
   ClassicDevice toClassicDevice() =>
